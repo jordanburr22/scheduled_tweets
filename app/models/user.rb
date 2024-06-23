13e611c25ca98029a56889cae62bf4ad1ddae403
Jordan <jordanburr23@gmail.com>
2024-06-23 00:41:40 -0400

    Added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, format: { with: /\^[^@]+@[^@]+\.[^@]+$/, message: "must be a valid email address" }
 end
