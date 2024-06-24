c8a7cc43a9d6654f768165433826a8c28582e355
Jordan <jordanburr23@gmail.com>
2024-06-24 00:25:01 -0400

    Added log in functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true, format: { with: /\^[^@]+@[^@]+\.[^@]+$/, message: "must be a valid email address" }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
 
+  get "sign_up", to: "registrations#new" 
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
-  
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 end
